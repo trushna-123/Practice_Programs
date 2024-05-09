742cec76fa5464ec4f69ae524b59e11aa53c147e
trushna khedkar <117332331+trushna-123@users.noreply.github.com>
2024-05-09 14:07:39 +0530

    Add files via upload

---
+++ Number_Pattern.java
@@ -0,0 +1,21 @@
+package com.pattern;
+
+import java.util.Scanner;
+
+public class Number_Pattern {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+//		int n = sc.nextInt();
+		
+		for (int i = 1; i <=m ; i++) {
+			
+			for (int j = 1; j < i; j++) {
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+}
